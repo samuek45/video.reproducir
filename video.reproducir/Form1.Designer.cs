c9873a1842f28256fd305d897b8d79abb15a7dee
Usuario <Usuario@B13-205-22953>
2022-09-27 11:14:24 -0500

    Agregar archivos de proyecto.

---
+++ video.reproducir/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace video.reproducir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.btncargar = new System.Windows.Forms.Button();
+            this.btnreproducir = new System.Windows.Forms.Button();
+            this.btnparar = new System.Windows.Forms.Button();
+            this.btnpausa = new System.Windows.Forms.Button();
+            this.lblruta = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(95, -2);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(1014, 264);
+            this.axWindowsMediaPlayer1.TabIndex = 4;
+            // 
+            // btncargar
+            // 
+            this.btncargar.Location = new System.Drawing.Point(95, 284);
+            this.btncargar.Name = "btncargar";
+            this.btncargar.Size = new System.Drawing.Size(75, 23);
+            this.btncargar.TabIndex = 5;
+            this.btncargar.Text = "cargar";
+            this.btncargar.UseVisualStyleBackColor = true;
+            this.btncargar.Click += new System.EventHandler(this.btncargar_Click);
+            // 
+            // btnreproducir
+            // 
+            this.btnreproducir.Location = new System.Drawing.Point(195, 284);
+            this.btnreproducir.Name = "btnreproducir";
+            this.btnreproducir.Size = new System.Drawing.Size(75, 23);
+            this.btnreproducir.TabIndex = 6;
+            this.btnreproducir.Text = "reproducir";
+            this.btnreproducir.UseVisualStyleBackColor = true;
+            this.btnreproducir.Click += new System.EventHandler(this.btnreproducir_Click);
+            // 
+            // btnparar
+            // 
+            this.btnparar.Location = new System.Drawing.Point(302, 284);
+            this.btnparar.Name = "btnparar";
+            this.btnparar.Size = new System.Drawing.Size(75, 23);
+            this.btnparar.TabIndex = 7;
+            this.btnparar.Text = "parar";
+            this.btnparar.UseVisualStyleBackColor = true;
+            this.btnparar.Click += new System.EventHandler(this.btnparar_Click);
+            // 
+            // btnpausa
+            // 
+            this.btnpausa.Location = new System.Drawing.Point(404, 284);
+            this.btnpausa.Name = "btnpausa";
+            this.btnpausa.Size = new System.Drawing.Size(75, 23);
+            this.btnpausa.TabIndex = 8;
+            this.btnpausa.Text = "pausa";
+            this.btnpausa.UseVisualStyleBackColor = true;
+            this.btnpausa.Click += new System.EventHandler(this.btnpausa_Click);
+            // 
+            // lblruta
+            // 
+            this.lblruta.AutoSize = true;
+            this.lblruta.Location = new System.Drawing.Point(30, 363);
+            this.lblruta.Name = "lblruta";
+            this.lblruta.Size = new System.Drawing.Size(35, 13);
+            this.lblruta.TabIndex = 9;
+            this.lblruta.Text = "label1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "todos|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1232, 595);
+            this.Controls.Add(this.lblruta);
+            this.Controls.Add(this.btnpausa);
+            this.Controls.Add(this.btnparar);
+            this.Controls.Add(this.btnreproducir);
+            this.Controls.Add(this.btncargar);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "leerpodf";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button btncargar;
+        private System.Windows.Forms.Button btnreproducir;
+        private System.Windows.Forms.Button btnparar;
+        private System.Windows.Forms.Button btnpausa;
+        private System.Windows.Forms.Label lblruta;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ video.reproducir/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace video.reproducir
+{
+    public partial class Form1 : Form
+    {
+        private string ruta = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblparar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncargar_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                ruta = openFileDialog1.FileName;
+                lblruta.Text = ruta;
+            }
+           
+
+        }
+
+        private void btnreproducir_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = ruta;
+            axWindowsMediaPlayer1.Ctlcontrols.play();        }
+
+        private void btnparar_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.stop();        }
+
+        private void btnpausa_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.pause();        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
